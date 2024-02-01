2a7d2b6cf389d76f38c85c3982df5b600ebe6988
Rajeevraj1234 <rarajeevraj@gmail.com>
2024-02-01 23:50:59 +0530

    Notes section CRUD complete

--- backend/Routes/notesRoute.js
+++ backend/Routes/notesRoute.js
@@ -1,31 +1,99 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const User = require("../Schema/userSchema");
 const Notes = require("../Schema/notesSchema");
-const {authMiddleware}  = require("../Middleware/auth");
-
+const { authMiddleware } = require("../Middleware/auth");
 
 //Create
-router.post("/createNote",authMiddleware,async(req,res)=>{
-    if(!req.body.title || !req.body.description || !req.body.visibility ){
-        res.status(400).json({
-            message:"Provide all detail"
-        })
+router.post("/createNote", authMiddleware, async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.description || !req.body.visibility) {
+      res.status(400).json({
+        message: "Provide all detail",
+      });
     }
     const note = await Notes.create({
-        title:req.body.title,
-        description:req.body.description,
-        visibility:req.body.visibility,
-    })
-    const data = await User.updateOne({_id:req.userDetail.userId},{
-        $push:{
-            notesRef:req.userDetail.userId
-        }
-    })
-    console.log(data);
-    res.json({note});
-})
+      title: req.body.title,
+      description: req.body.description,
+      visibility: req.body.visibility,
+    });
+    await User.updateOne(
+      { _id: req.userDetail.userId },
+      {
+        $push: {
+          notesRef: note._id,
+        },
+      }
+    );
+    res.status(200).json({
+      message: "Notes Created Successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
 
+//Update
 
+router.put("/updateNote/:noteId", authMiddleware, async (req, res) => {
+  try {
+    const { noteId } = req.params;
+
+    if (!req.body) {
+      res.status(400).json({
+        message: "No Inputs",
+      });
+    }
+    await Notes.updateOne({ _id: noteId }, req.body);
+    res.status(200).json({
+      message: "Successfully Updated",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
+
+//Delete
+router.delete("/deleteNote/:noteId", authMiddleware, async (req, res) => {
+  try {
+    const { noteId } = req.params;
+    await Notes.findByIdAndDelete(noteId);
+    await User.findByIdAndUpdate(req.userDetail.userId, {
+      $pull: {
+        notesRef: noteId,
+      },
+    });
+    res.status(200).json({
+      message: "Successfully Deleted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
+
+// Get All Data
+router.get("/allNotes", async (req, res) => {
+  try {
+    const allNotes = await Notes.find({ visibility: "public" });
+    if (allNotes == {}) {
+      res.status(404).json({
+        message: "No Notes Available",
+      });
+    }
+    res.status(200).json({
+      allNotes,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
 
 module.exports = router;
